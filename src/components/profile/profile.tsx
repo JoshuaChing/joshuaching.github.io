15cfa3a8f9f03dcb0e2bce460b858e8ee304770e
Joshua Ching <jchingdev@gmail.com>
2023-01-07 15:43:42 -0800

    profile picture styling

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -2,16 +2,20 @@ import * as React from "react"
 
 const styles = require('./profile.module.scss')
 
+const headerText = "Joshua Ching"
+const subheaderText = "Software Engineer"
+const picture = "👨‍💻"
+
 class Profile extends React.PureComponent {
     render() {
         return (
-            <div>
+            <div className={styles.profileContainer}>
                 <div className={styles.headerContainer}>
-                    <div className={styles.headerText}>Joshua Ching</div>
-                    <div className={styles.subheaderText}>Software Engineer</div>
+                    <div className={styles.headerText}>{headerText}</div>
+                    <div className={styles.subheaderText}>{subheaderText}</div>
                 </div>
                 <div className={styles.pictureContainer}>
-                    <div className={styles.picture}>👨‍💻</div>
+                    <div className={styles.picture}>{picture}</div>
                 </div>
             </div>
         )
