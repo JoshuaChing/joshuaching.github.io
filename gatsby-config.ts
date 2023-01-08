249f158ff8f8b7a873ed6e4fc80a97b335c506b3
Joshua Ching <jchingdev@gmail.com>
2023-01-07 20:10:09 -0800

    get ready to deploy github pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `joshuaching.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `joshuaching.github.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
