6d10c54169ee6c2f91eadee16694568d9af702bb
Joshua Ching <jchingdev@gmail.com>
2023-01-07 16:52:41 -0800

    some more styling and color selection

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -4,21 +4,23 @@ const styles = require('./profile.module.scss')
 
 const headerText = "Joshua Ching"
 const subheaderText = "Software Engineer"
-const picture = "👨‍💻"
+const picture = "👨🏻‍💻"
 const description = "Currently working at Microsoft. Grateful for my past experiences at Yelp and Shopify."
 
 class Profile extends React.PureComponent {
     render() {
         return (
-            <div className={styles.profileContainer}>
-                <div className={styles.headerContainer}>
-                    <div className={styles.headerText}>{headerText}</div>
-                    <div className={styles.subheaderText}>{subheaderText}</div>
+            <div>
+                <div className={styles.profileContainer}>
+                    <div className={styles.headerContainer}>
+                        <div className={styles.headerText}>{headerText}</div>
+                        <div className={styles.subheaderText}>{subheaderText}</div>
+                    </div>
+                    <div className={styles.pictureContainer}>
+                        <div className={styles.picture}>{picture}</div>
+                    </div>
                 </div>
-                <div className={styles.pictureContainer}>
-                    <div className={styles.picture}>{picture}</div>
-                </div>
-                {/* <div>{description}</div> */}
+                <p>{description}</p>
             </div>
         )
     }
