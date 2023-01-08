47ae4ed8269b03d2e07e472503691db8c4286184
Joshua Ching <jchingdev@gmail.com>
2023-01-07 22:06:08 -0800

    fix font awesome flashing resizing bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import type { HeadFC } from "gatsby"
 import Profile from "../components/profile/profile"
 import ProfileButtons from "../components/profileButtons/profileButtons"
 
+// snippet to fix flashing resizing font awesome icons
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 require('../common/app.module.scss')
 const styles = require('../common/common.module.scss')
 
